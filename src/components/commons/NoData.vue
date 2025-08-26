f881eefa06d974d4c262b43574b8b3560e48b12c
Wiby Fabian <wibyfabian08@gmail.com>
2025-08-26 13:13:36 +0700

    fix

--- src/components/commons/NoData.vue
+++ src/components/commons/NoData.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col items-center py-10">
-    <img src="../../assets/images/NoData.svg" alt="no-data" class="mb-5" />
-    <p class="font-semibold text-s text-netral-100">Belum ada data</p>
+    <img src="../../assets/images/NoData.svg" alt="no-data" class="object-contain h-24 mb-4" />
+    <p class="font-semibold text-m text-netral-100">Belum ada data</p>
   </div>
 </template>
